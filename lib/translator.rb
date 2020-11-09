4ba6d808e3abdd177be5190b191d3d401fa3b4b8
Elvis Landi <elandi13@yahoo.com>
2020-11-09 19:42:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   new_hash = {}
-emoticons.each do |key,value|
+ emoticons.each do |key,value|
   new_hash[key] = {}
   new_hash[key][:english] = value[0]
   new_hash[key][:japanese] = value[1]
